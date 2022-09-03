cae3e9c65736c00ad2e0a0ba35cbfd9d5f45fa22
Luiz Filipe dos Santos Paula <fii.lfsp@gmail.com>
2022-09-03 03:24:46 -0300

    teste exibição

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,16 +1,8 @@
 function tempo(request, response) {
-    const apiSecret =process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
-
-    const usuarios = await fetch ("https://api.github.com/users");
-    const usuariosJson = usuarios.json();
-    const login = usuariosJson.login;
-    const foto = usuariosJson.avatar_url;
 
     response.json({
         date: dynamicDate.toGMTString(),
-        usuarios: login,
-        foto: foto
     })
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import tempo from "./api/tempo";
 function Home() {
   return (
     <div>
       <h1>Contatos</h1>
+      <h2>sabe os dados de tempo neste exato momento?</h2>
+      <h3>Agora sabe:</h3>
+      <p>{tempo}</p>
     </div>
 
   );
